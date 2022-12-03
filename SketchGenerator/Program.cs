7e937c80e40395036386e7c06cfac47d5701617c
Pointer Guide <pointerguide@gmail.com>
2022-12-03 23:32:28 +0100

    Update Program.cs

--- SketchGenerator/Program.cs
+++ SketchGenerator/Program.cs
@@ -7,7 +7,7 @@ const int batchSize = 128;
 const long noiseDimensions = 50;
 const long imageOutputSize = 28 * 28 * 1;
 const double optimizersLr = 0.0002;
-const (double, double) optimizersBetas = (0.5, 0.999);
+(double, double) optimizersBetas = (0.5, 0.999);
 
 //Create models
 NetworksTrainer networksTrainer = new NetworksTrainer(DeviceType.CUDA, noiseDimensions, imageOutputSize, optimizersLr, optimizersBetas);
