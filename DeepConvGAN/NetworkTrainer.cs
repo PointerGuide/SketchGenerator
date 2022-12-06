eb13ad5781c2f09842f727356d28c8d19a0564ec
Pointer Guide <pointerguide@gmail.com>
2022-12-06 01:27:58 +0100

    dataset manipulations

--- DeepConvGAN/NetworkTrainer.cs
+++ DeepConvGAN/NetworkTrainer.cs
@@ -16,11 +16,14 @@ namespace DeepConvGAN
         internal Tensor LoadAndPreprocessDataset()
         {
             NDArray dataset = np.load(@"Dataset/airplane.npy");
-            Tensor ten = from_array(dataset.ToMuliDimArray<byte>()).@float().cuda();
+            Tensor ten = from_array(dataset.ToMuliDimArray<byte>()).@float();
             ten = (ten - 127.5) / 127.5; //Normalizacja
             ten = ten.reshape(ten.size(0), 28, 28);
-
+            ten = transforms.Pad(new long[] { 2, 2, 2, 2 }, fill: -1).forward(ten);
             //ten = ten[TensorIndex.Slice(0, 125), TensorIndex.Colon];
+            var test = ten[0];
+            test = test * 127.5 + 127.5;
+            io.write_png(test.@byte().unsqueeze(0), "testUpsacled.png");
             return ten;
         }
     }
